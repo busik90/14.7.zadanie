757ed36074f72d9f9c9131edc98378db39c9b398
Pawel Bus <pawelbus.dev@outlook.com>
2018-07-05 12:24:30 +0200

    Change code in App.js to JSX

--- components/App.js
+++ components/App.js
@@ -28,10 +28,10 @@ var contactForm = {
 var App = React.createClass({
   render: function() {
     return (
-      React.createElement('div', {className: 'app'},
-        React.createElement(ContactForm, {contact: contactForm}),
-        React.createElement(Contacts, {items: contacts}, {}) // Po co jest drugi nawis klamrowy? Wyżej go nie ma.
-      )
+      <div className='app'>
+        <ContactForm contact={contactForm} />
+        <Contacts items={contacts} />
+      </div>
     );
   }
 });
